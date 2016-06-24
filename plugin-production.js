074066791421608f013f994f607943d8ec3ad238
casperlamboo <casperlamboo@gmail.com>
2016-06-24 11:05:24 +0200

    implement production

--- plugin-production.js
+++ plugin-production.js
@@ -1,7 +1,23 @@
-var workerFunctions = ['terminate', 'postMessage', 'addEventListener', 'removeEventListener'];
+exports.fetch = function(load) {
+  var workerCode = [
+    'self.addEventListener("message", function(event) {',
+    '  self.postMessage(event.data);',
+    '}, false);'
+  ].join('\n');
 
-function createWorker(load) {
-
+  return workerCode;
 }
 
-module.exports = createWorker;
+exports.translate = function(load) {
+  load.metadata.format = 'esm';  
+
+  return [
+    'const codeStr = ' + JSON.stringify(load.source) + ';',
+    'const blob = new Blob([codeStr]);',
+    'const blobURL = self.URL.createObjectURL(blob);',
+
+    'export default function() {',
+    '  return new Worker(blobURL);',
+    '}'
+  ].join('\n');
+};
