83ef1d69859f230e8bf645ed314afa787cebc424
casperlamboo <casperlamboo@gmail.com>
2016-06-24 10:03:05 +0200

    only export when is defined

--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
 // map is set to load plugin-dev in development and plugin-production in production
 var plugin = require('./plugin.js');
 
-exports.locate = plugin.locate;
-exports.fetch = plugin.fetch;
-exports.translate = plugin.translate;
-exports.instantiate = plugin.instantiate;
+if (plugin.locate !== undefined) exports.locate = plugin.locate;
+if (plugin.fetch !== undefined) exports.fetch = plugin.fetch;
+if (plugin.translate !== undefined) exports.translate = plugin.translate;
+if (plugin.instantiate !== undefined) exports.instantiate = plugin.instantiate;
