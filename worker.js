76298d4a99f57757560ec8c2e470a8fe84af960a
casperlamboo <casperlamboo@gmail.com>
2016-06-24 12:21:52 +0200

    update comments

--- worker.js
+++ worker.js
@@ -1,8 +1,9 @@
 // map is set to load plugin-dev in development and plugin-production in production
 var plugin = require('./plugin.js');
 
-// re export all function from plugin (this is kinda lame)
-// wanted to apply conditinal loading on the entry point of the plugin but that is not possible
+// because it doesn't seem possible to conditionally load either the
+// production or development code as the entry point (main in package.json)
+// we had to add this in between step.
 if (plugin.locate !== undefined) exports.locate = plugin.locate;
 if (plugin.fetch !== undefined) exports.fetch = plugin.fetch;
 if (plugin.translate !== undefined) exports.translate = plugin.translate;
