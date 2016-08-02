f23403784bb25b2114d13cc45dec0ca8b9fc1500
casperlamboo <casperlamboo@gmail.com>
2016-08-02 15:19:26 +0200

    typo

--- src/loader-dev.js
+++ src/loader-dev.js
@@ -1,8 +1,8 @@
 exports.fetch = function(load) {
   // create code string to be used in the worker
   // code loads the worker in load.address
-  // when it is fineshed loading the worker post @@@LOADED@@@
   // note: the system.src.js and jspm.config.js can be located differently, not sure how to handle this
+  // when it is finished loading the worker post @@@LOADED@@@
   var codeStr = [
     'importScripts("' + System.baseURL + 'jspm_packages/system.src.js");',
     'System.config(' + JSON.stringify(System.getConfig()) + ');',
