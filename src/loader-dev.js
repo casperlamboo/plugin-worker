39d6a9a5fcd63bb2b40ab789c3c1f88ee3807a43
casperlamboo <casperlamboo@gmail.com>
2016-08-02 15:19:32 +0200

    update config

--- src/loader-dev.js
+++ src/loader-dev.js
@@ -1,8 +1,8 @@
 exports.fetch = function(load) {
   // create code string to be used in the worker
   // code loads the worker in load.address
-  // note: the system.src.js and jspm.config.js can be located differently, not sure how to handle this
   // when it is finished loading the worker post @@@LOADED@@@
+  // note: jspm.config.js can be located differently, not sure how to handle this
   var codeStr = [
     'importScripts("' + System.baseURL + 'jspm_packages/system.src.js");',
     'System.config(' + JSON.stringify(System.getConfig()) + ');',
