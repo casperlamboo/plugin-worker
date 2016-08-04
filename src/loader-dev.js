9e02236402497e0f6d316dd71b79fed3f83f4186
casperlamboo <casperlamboo@gmail.com>
2016-08-04 15:56:39 +0200

    use JSON.stringify in concatenation of strings

--- src/loader-dev.js
+++ src/loader-dev.js
@@ -4,11 +4,11 @@ exports.fetch = function(load) {
   // when it is finished loading the worker post @@@LOADED@@@
   // note: jspm.config.js can be located differently, not sure how to handle this
   var codeStr = [
-    'importScripts("' + System.baseURL + 'jspm_packages/system.src.js");',
+    'importScripts(' + JSON.stringify(System.baseURL) + ' + "jspm_packages/system.src.js");',
     'System.config(' + JSON.stringify(System.getConfig()) + ');',
 
     // Because System.import is async we need a worker shim to catch all messages
-    'System.import("' + load.address + '").then(function() {',
+    'System.import(' + JSON.stringify(load.address) + ').then(function() {',
     '  self.postMessage("@@@LOADED@@@")',
     '}).catch(function(error) {',
     '  console.warn(error);',
